07ceb9ef122703075ab5aac19fcfe8c0e820cf90
João Luís A. <kalliddel@gmail.com>
2024-03-15 15:24:49 -0300

    Hamburger menu WIP

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+// Hamburger menu stuff
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navigator-design")
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+
+})
