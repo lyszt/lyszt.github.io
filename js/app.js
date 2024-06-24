401f08c1ca20cbd00ea4382ff6b7e1e5745a31f5
lyszt <kalliddel@gmail.com>
2024-06-24 17:03:12 -0300

    Very big change on portfolio section. New design created with a wireframe made before. Good practice

--- js/app.js
+++ js/app.js
@@ -12,10 +12,40 @@ hamburger.addEventListener("click", () => {
 
 // Menu item interactions on portfolio pick
 
-const port_item = document.QuerySelector(".port-option");
-const port_img = document.QuerySelector(".port-option img")
-const port_btn = document.QuerySelector(".port-option a h3")
-port_item.addEventListener("hover", () => {
-  port_img.classList.toggle("hover");
-  port_btn.classList.toggle("hover");
+const photography_button = document.querySelector("#port-photography")
+const videography_button = document.querySelector("#port-video")
+const design_button = document.querySelector("#port-web")
+const art_button = document.querySelector("#port-art")
+
+const photo_folder = document.querySelector("#photography-folder")
+const video_folder = document.querySelector("#video-folder")
+const design_folder = document.querySelector("#web-folder")
+const art_folder = document.querySelector("#art-folder")
+
+
+photography_button.addEventListener("click", () => {
+  photo_folder.classList.remove("hidden")
+  video_folder.classList.add("hidden")
+  design_folder.classList.add("hidden")
+  art_folder.classList.add("hidden")
 })
+videography_button.addEventListener("click", () => {
+  video_folder.classList.remove("hidden")
+  photo_folder.classList.add("hidden")
+  design_folder.classList.add("hidden")
+  art_folder.classList.add("hidden")
+})
+design_button.addEventListener("click", () => {
+  design_folder.classList.remove("hidden")
+  photo_folder.classList.add("hidden")
+  video_folder.classList.add("hidden")
+  art_folder.classList.add("hidden")
+})
+art_button.addEventListener("click", () => {
+  art_folder.classList.remove("hidden")
+  photo_folder.classList.add("hidden")
+  video_folder.classList.add("hidden")
+  design_folder.classList.add("hidden")
+})
+
+
