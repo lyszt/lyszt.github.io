7ad7d637bfd0f23dd485541ad959184b4b01a25c
lyszt <kalliddel@gmail.com>
2024-07-01 10:28:13 -0300

    Art folder removed and replaced by coding projects folder

--- js/app.js
+++ js/app.js
@@ -16,34 +16,34 @@ hamburger.addEventListener("click", () => {
 const photography_button = document.querySelector("#port-photography")
 const videography_button = document.querySelector("#port-video")
 const design_button = document.querySelector("#port-web")
-const art_button = document.querySelector("#port-art")
+const code_button = document.querySelector("#port-code")
 
 const photo_folder = document.querySelector("#photography-folder")
 const video_folder = document.querySelector("#video-folder")
 const design_folder = document.querySelector("#web-folder")
-const art_folder = document.querySelector("#art-folder")
+const code_folder = document.querySelector("#code-folder")
 
 
 photography_button.addEventListener("click", () => {
   photo_folder.classList.remove("hidden")
   video_folder.classList.add("hidden")
   design_folder.classList.add("hidden")
-  art_folder.classList.add("hidden")
+  code_folder.classList.add("hidden")
 })
 videography_button.addEventListener("click", () => {
   video_folder.classList.remove("hidden")
   photo_folder.classList.add("hidden")
   design_folder.classList.add("hidden")
-  art_folder.classList.add("hidden")
+  code_folder.classList.add("hidden")
 })
 design_button.addEventListener("click", () => {
   design_folder.classList.remove("hidden")
   photo_folder.classList.add("hidden")
   video_folder.classList.add("hidden")
-  art_folder.classList.add("hidden")
+  code_folder.classList.add("hidden")
 })
-art_button.addEventListener("click", () => {
-  art_folder.classList.remove("hidden")
+code_button.addEventListener("click", () => {
+  code_folder.classList.remove("hidden")
   photo_folder.classList.add("hidden")
   video_folder.classList.add("hidden")
   design_folder.classList.add("hidden")
