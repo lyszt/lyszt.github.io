c042330338063c81f3795840bfdd1c58f24f1c8a
lyszt <kalliddel@gmail.com>
2024-11-06 21:46:15 -0300

    optimization

--- js/translator.js
+++ js/translator.js
@@ -18,24 +18,17 @@ addEventListener("DOMContentLoaded", () => {
   // Elements to translate in homepage
   const user_lang = navigator.language;
   console.log(`User language is ${user_lang}`);
-
-// Wait for the iframe to load
-
-  // Append the style element to the head of the document
-  document.head.appendChild(style);
   const cv_link = document.querySelector(".cv");
 
   // FRENCH
   if (user_lang.startsWith("fr")) {
     cv_link.href = "https://drive.google.com/file/d/1UJTMjMxV83N_CFwpVpyr34CVuklIPgcJ/view?usp=sharing";
-    }
-    // PORTUGUESE
-  } else if (user_lang.startsWith("pt")) {
+  }
+  else if (user_lang.startsWith("pt")) {
     cv_link.href = "https://drive.google.com/file/d/1FK_yAh3cft7x2CrK-GnOr5fbjv-heKwg/view?usp=sharing";
   } else {
     cv_link.href = "https://drive.google.com/file/d/1yEAZPgopRYWOElEYl4y3Mi0A1oyE5U2D/view?pli=1";
   }
-
   if (!user_lang.startsWith("en")) {
     console.log(`Detected language (${user_lang}) is not English.`);
       
