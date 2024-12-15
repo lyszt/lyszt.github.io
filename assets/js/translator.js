2a136b549ca6a0b0aa3889d924872c2321ef0401
João Luís A. <116316121+lyszt@users.noreply.github.com>
2024-12-15 00:25:41 -0300

    change querySelector of CV to querySelectorAll

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -1,17 +1,5 @@
 // TRANSLATION
 
-const photography_button = document.querySelector("#port-photography a");
-const videography_button = document.querySelector("#port-video a");
-const design_button = document.querySelector("#port-web a");
-const code_button = document.querySelector("#port-code a");
-
-const photo_folder = document.querySelector("#photography-folder");
-const video_folder = document.querySelector("#video-folder");
-const design_folder = document.querySelector("#web-folder");
-const code_folder = document.querySelector("#code-folder");
-
-
-
 addEventListener("DOMContentLoaded", () => {
 
   // GeoLocation interactions
