85c736f5525d77a726e073db1deeae526f610322
João Luís A. <116316121+lyszt@users.noreply.github.com>
2024-12-15 00:25:21 -0300

    change querySelector of CV to querySelectorAll

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -15,26 +15,23 @@ const code_folder = document.querySelector("#code-folder");
 addEventListener("DOMContentLoaded", () => {
 
   // GeoLocation interactions
-  // Elements to translate in homepage
   const user_lang = navigator.language;
   console.log(`User language is ${user_lang}`);
   const cv_links = document.querySelectorAll(".cv");
 
-  // Check if cv_links is not empty
-  if (cv_links.length > 0) {
-    // FRENCH
-    if (user_lang.startsWith("fr")) {
-      cv_links.forEach(link => link.href = "https://drive.google.com/file/d/1UJTMjMxV83N_CFwpVpyr34CVuklIPgcJ/view?usp=sharing");
-    }
-    // PORTUGUESE
-    else if (user_lang.startsWith("pt")) {
-      cv_links.forEach(link => link.href = "https://drive.google.com/file/d/1FK_yAh3cft7x2CrK-GnOr5fbjv-heKwg/view?usp=sharing");
-    }
-    // Default (English or others)
-    else {
-      cv_links.forEach(link => link.href = "https://drive.google.com/file/d/1yEAZPgopRYWOElEYl4y3Mi0A1oyE5U2D/view?pli=1");
-    }
+  // FRENCH
+  if (user_lang.startsWith("fr")) {
+    cv_links.forEach(link => link.href = "https://drive.google.com/file/d/1UJTMjMxV83N_CFwpVpyr34CVuklIPgcJ/view?usp=sharing");
   }
+  // PORTUGUESE
+  else if (user_lang.startsWith("pt")) {
+    cv_links.forEach(link => link.href = "https://drive.google.com/file/d/1FK_yAh3cft7x2CrK-GnOr5fbjv-heKwg/view?usp=sharing");
+  }
+  // Default (English or others)
+  else {
+    cv_links.forEach(link => link.href = "https://drive.google.com/file/d/1yEAZPgopRYWOElEYl4y3Mi0A1oyE5U2D/view?pli=1");
+  }
+
 
   // Log if the language is not English
   if (!user_lang.startsWith("en")) {
