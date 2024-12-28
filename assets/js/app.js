c49318a524cdafa07464f6aed7dd3a878cdbe92a
João Luís A. <116316121+lyszt@users.noreply.github.com>
2024-12-27 21:08:56 -0300

    updating js for functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -88,8 +88,8 @@ addEventListener("DOMContentLoaded", () => {
   if(transition_anim.classList.contains("active")){
     transition_anim.classList.remove("active");
   }
-  design_folder.classList.remove("hidden");
-  design_button.classList.add("focus");
+  code_folder.classList.remove("hidden");
+  code_button.classList.add("focus");
 
   // Landing page loop animations
   let timer = 10000;
